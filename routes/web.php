408fecfedb1366d95b747edd02b06f5a33c3db8a
3A832057 <qoooppadsl@gmail.com>
2021-11-14 23:02:55 +0800

    使用destroy方法刪除id=2的資料

--- routes/web.php
+++ routes/web.php
@@ -48,8 +48,10 @@ use App\Models\Post;
 //$post->content='saved content';
 //$post->save();
 
-$post=Post::find(1);
-$post->delete();
+//$post=Post::find(1);
+//$post->delete();
+
+Post::destroy(2);
 
 
 Route::get('/', function () {
