d25b1a4f47643441bfb0e81be5053db291742f5d
3A832057 <qoooppadsl@gmail.com>
2021-11-14 21:52:59 +0800

    使用where方式取出posts中符合條件的資料

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,10 @@ use App\Models\Post;
 //$posts=Post::all();
 //dd($posts);
 
-$post=Post::find(1);
-dd($post);
+//$post=Post::find(1);
+//dd($post);
+$posts=Post::where('id','<','10')->orderBy('id','DESC')->get();
+dd($posts);
 
 Route::get('/', function () {
     return view('index');
