79bf68e10a147c82c1ea52a7c6317b01419ea100
3A832057 <qoooppadsl@gmail.com>
2021-11-14 22:31:46 +0800

    使用all方法抓出posts資料表中所有資料(補十筆資料)

--- routes/web.php
+++ routes/web.php
@@ -26,14 +26,11 @@ use App\Models\Post;
 //    'title' =>'test title',
 //    'content' =>'test content',
 //]);
-
-//$posts=Post::all();
-//dd($posts);
+$posts=Post::all();
+dd($posts);
 
 //$post=Post::find(1);
 //dd($post);
-$posts=Post::where('id','<','10')->orderBy('id','DESC')->get();
-dd($posts);
 
 Route::get('/', function () {
     return view('index');
